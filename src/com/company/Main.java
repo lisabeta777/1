b0e346e22110a6db480861bc0c3081ccabea21af
Елизавета <lisabeta777@mail.ru>
2021-03-14 11:34:36 +0300

    исправления

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,40 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        String phone = "89117237096";
-        VerifyNumberPhone(phone);
 
+        String phone = "+791172 3) 7096";
+        System.out.println(Arrays.toString(VerifyNumberPhone(phone)));
     }
-    public static void VerifyNumberPhone (String phone) {
 
-       String phone_1 = phone.replaceFirst("\\+7", "8");
-       String phone_2 = phone_1.replace(" ", "");
-       String phone_3 = phone_2.replace("(",  "");
-       String phone_4 = phone_3.replace(")",  "");
+    public static String[] VerifyNumberPhone (String phone) {
 
+        String phone_1 = phone.replaceFirst("\\+7", "8");
+        String phone_2 = phone_1.replace(" ", "");
+        String phone_3 = phone_2.replace("(", "");
+        String phone_4 = phone_3.replace(")", "");
+
+        String[] array  = {"", ""};
         int length = phone_4.length();
-        if (length > 11 ){
-            System.out.println("Введен некорректный номер; Количество символов больше 11");}
-        else {
-            System.out.println(phone_4);
+        if (length > 11) {
+            array[0] = "Введен некорректный номер; Количество символов больше 11";
+        } else {
+            array[0] = phone_4;
 
-            if (phone_1 != phone) {
-                System.out.print("Замена +7 на 8; ");
+            if (!phone_1.equals(phone)) {
+                array[1] = "Замена +7 на 8; ";
             }
-            if (phone_1 != phone_4) {
-                System.out.print("В номере есть пробелы и/или скобки");
+            if (!phone_1.equals(phone_4)) {
+                array[1] = array[1] + "В номере есть пробелы и/или скобки";
             }
-            if (phone == phone_4) {
-                System.out.print("Не было изменений");
+            if (phone.equals(phone_4)) {
+                array[1] = "Не было изменений";
             }
         }
-
+        return array;
     }
+
 }
